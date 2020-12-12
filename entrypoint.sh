9b24ed27fc0dd4fe059ddb7b7a0ccd9716c0b9d7
Jordan <jordan.wallace.williams@gmail.com>
2020-12-12 11:58:47 -0700

    Update entrypoint to only run Airflow Scheduler

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@
 
 cd /projects
 
+# meltano config meltano set database_uri "postgresql://postgres:OniFbN6Jwbucr5MI@/meltano-meta?host=/cloudsql/gadmin-reports-294123:us-central1:gitlab"
+# meltano invoke airflow upgradedb
+# meltano config airflow list
 meltano invoke airflow scheduler
